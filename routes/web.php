6f31be4a611348cec02ce6c3831b337a4e134156
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2023-06-01 08:34:40 -0500

    Create author resource.

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AutorController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::resource('/autores', AutorController::class);
