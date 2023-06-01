666da40a8838e6f950290d903b9425fb930e4b58
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2023-06-01 13:06:17 -0500

    adjust buttons positions.

--- resources/views/Autores/create.blade.php
+++ resources/views/Autores/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.base')
 @section('content')
-    <a class="btn btn-primary" href="/autores">Regresar</a>
     <form action="/autores" method="POST">
         @csrf
         <div class="mb-3">
             <label for="nombre">Nombre:</label>
             <input type="text" class="form-control" id="nombre" name="nombre">
         </div>
+        <a class="btn btn-primary" href="/autores">Regresar</a>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 @endsection
--- resources/views/Autores/edit.blade.php
+++ resources/views/Autores/edit.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.base')
 @section('content')
-    <a class="btn btn-primary" href="/autores">Regresar</a>
     <form action="/autores/{{ $autor->codigo }}" method="POST">
         @csrf
         @method('put')
@@ -8,6 +7,7 @@
             <label for="nombre">Nombre:</label>
             <input type="text" class="form-control" id="nombre" name="nombre" value="{{$autor->nombre}}">
         </div>
+        <a class="btn btn-primary" href="/autores">Regresar</a>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 @endsection
